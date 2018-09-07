9239f9d6c182ec01b0651b085c0ecf133608fe88
Michael Plant <mplant@trenethick.co.uk>
2018-09-07 16:01:37 +0100

    Add files via upload

--- resources/views/emails/admin/swap.blade.php
+++ resources/views/emails/admin/swap.blade.php
@@ -1,13 +1,13 @@
 @component('mail::message')
 # Request
-{{ $requests[0]->first_name }} requested to swap with {{ $requests[1]->first_name }} for:
+{{ $requests[0]->first_name }} requested to swap with you for:
 
 {{ $requests[2]->roles->role }} {{ $requests[0]->first_name }} {{ $requests[0]->last_name }} {{ $requests[2]->start_time }} {{ $requests[2]->finish_time }}
 
 on {{ \Carbon\Carbon::parse($requests[2]->days->date)->format('l d F Y') }}
 
 @component('mail::button', ['url' => ''])
-Approve
+Agree
 @endcomponent
 
 @component('mail::button', ['url' => ''])
